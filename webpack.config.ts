59952a80ae02de7caf1bb13388e6e45810824fca
Tyler Long <tyler4long@gmail.com>
2020-06-12 19:25:35 -0700

    html-webpack-plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,16 @@
-const config = {
+/* eslint-disable node/no-unpublished-import */
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  devtool: '#source-map',
   entry: './src/index.ts',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'GitHub Note',
+    }),
+  ],
 };
 
 export default [config];
