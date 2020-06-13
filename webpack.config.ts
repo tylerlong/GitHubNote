62f4c6bb6023257479185a5370ecf10ac0d1d310
Tyler Long <tyler4long@gmail.com>
2020-06-12 19:28:28 -0700

    Release version

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,15 @@
 /* eslint-disable node/no-unpublished-import */
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
+import path from 'path';
 
 const config: webpack.Configuration = {
   mode: 'development',
   devtool: '#source-map',
   entry: './src/index.ts',
+  output: {
+    path: path.join(__dirname, 'docs'),
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'GitHub Note',
