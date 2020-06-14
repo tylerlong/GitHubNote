59a7eb4faa26d649a3c47334c5affd2be6176c9a
Tyler Long <tyler4long@gmail.com>
2020-06-13 22:08:11 -0700

    react-subx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import react from 'react';
-import { ProxyObj } from 'subx/build/types';
+import React from 'react';
+import { SubxObj } from 'subx/build/types';
+import { Component } from 'react-subx';
 
-export interface Props { store: ProxyObj; }
+export interface Props { store: SubxObj; }
 
-class App extends react.Component<Props> {
+class App extends Component<Props> {
   render() {
     const store = this.props.store;
-    return store.hello;
+    return <>
+      <h1>{store.hello}</h1>
+      <button onClick={e => store.hello = 'clicked'}>click me</button>
+    </>;
   }
 }
 
